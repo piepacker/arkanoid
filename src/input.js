4e7149d92c36170083fcb3886fc7f6db577a45ca
Jean-André Santoni <jean.andre.santoni@gmail.com>
2022-07-06 13:26:26 +0200

    Fix wrong analog cb

--- src/input.js
+++ src/input.js
@@ -9,7 +9,7 @@ mergeInto(LibraryManager.library, {
       return Module.input_analogL_user_state[port][id];
   },
 
-  js_analogR_input_state: function (port, index, id) {
+  js_analogR_input_state: function (port, id) {
       return Module.input_analogR_user_state[port][id];
   },
 });
